2a508dfd4176d3d5b64cae119ba76cbed3099f1d
Jonathan Keslin <github@jonathankeslin.com>
2022-09-02 15:56:49 -0700

    Remove cypress project ID from hourly weather

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'o468vw',
   includeShadowDom: true,
   e2e: {
     baseUrl: 'http://localhost:8000/cypress/fixtures/'
